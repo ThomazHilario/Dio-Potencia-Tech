564d8926786b273d593f08bc486598c89507f744
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-11 12:28:15 -0300

    Desenvolvendo o layout do containerLeft

--- Jogos-PotenciaTech/Yu-Gi-Oh-Game/src/Components/MainGame/index.jsx
+++ Jogos-PotenciaTech/Yu-Gi-Oh-Game/src/Components/MainGame/index.jsx
@@ -1,4 +1,6 @@
 import './maingame.css'
+import {useContext} from 'react'
+import { Context } from '../../Context'
 
 export const MainGame = () => {
     return(
@@ -14,9 +16,24 @@ export const MainGame = () => {
 
 // Componente ContainerLeft 
 const ContainerLeft = () => {
+    // States globais - win e lose
+    const {contWin, contLoser} = useContext(Context)
     return(
         <div id="ContainerLeft">
-            oi
+
+            {/* Painel Profile */}
+            <div id='painelProfile'>
+                <h1>Win: <span>{contWin}</span></h1>
+                <h1>Loser: <span>{contLoser}</span></h1>
+            </div>
+
+            {/* Card */}
+            <div id='cardVisualization'>
+
+            </div>
+
+            {/* Attributes card */}
+            <div id='attributesCard'></div>
         </div>
     )
 }
