ef645d4a4d188407b548d10a929302e835b98bf6
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-11 13:43:36 -0300

    Desenvolvendo o ContainerRight
    - Fazendo requisição a uma api de yugioh para armazenar as cartas em uma state.
    
    - Criando uma função capaz cinco cartas aleatórias tanto para o bot quanto para o player.

--- Jogos-PotenciaTech/Yu-Gi-Oh-Game/src/Components/MainGame/index.jsx
+++ Jogos-PotenciaTech/Yu-Gi-Oh-Game/src/Components/MainGame/index.jsx
@@ -1,5 +1,5 @@
 import './maingame.css'
-import {useContext} from 'react'
+import {useContext, useState, useEffect} from 'react'
 import { Context } from '../../Context'
 
 export const MainGame = () => {
@@ -18,6 +18,7 @@ export const MainGame = () => {
 const ContainerLeft = () => {
     // States globais - win e lose
     const {contWin, contLoser} = useContext(Context)
+
     return(
         <div id="ContainerLeft">
 
@@ -40,9 +41,87 @@ const ContainerLeft = () => {
 
 // Componente ContainerRight 
 const ContainerRight = () => {
+
+    useEffect(() => {
+        // loadCards
+        async function loadCards(){
+            // Fazendo a requisicao
+            const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+
+            // Transformando o valor da requisicao para ser manipulavel
+            const data = await response.json()
+
+            // Filtrando o resultado para vim somente monstros
+            const cardsMonster = data.data.filter(element => element.type !== 'Spell Card')
+
+            // setando na state cards
+            setCards([...cardsMonster])
+
+        }
+
+        // Executando loadCards
+        loadCards()
+
+    },[])
+
+    // Cartas do jogo
+    const [cards, setCards] = useState([])
+
+    // state - myCards
+    const [myCards, setMyCards] = useState([])
+
+    // state - botCards
+    const [botCards, setBotCards] = useState([])
+
+    // Escolehndo cinco cartas random para o bot e para o player
+    function randomCards(array){
+
+        // Esvaziando array
+        if(array.length === 5){
+            for(let i = 0; i < 5; i++){
+                array.splice(i)
+            }
+        }
+
+        for(let i = 0; i < 5; i++){
+            array.push(cards[Math.floor(Math.random() * cards.length)])
+        }
+
+        
+    }
+
+    function getCards(){
+
+        // Chamando o randomCards para escolher as cartas do bot e setando na state
+        randomCards(botCards)
+        setBotCards([...botCards])
+
+        // Chamando o randomCards para escolher as cartas do player e setando na state
+        randomCards(myCards)
+        setMyCards([...myCards])
+
+    }
+
+    function startGame(){
+        // Chamando o getCards
+        getCards()
+
+        // Alterando o display do button para none
+        document.getElementById('btn-Start-Game').style.display = 'none'
+
+        // Alterando o display do ContainerLeft
+        document.getElementById('ContainerLeft').style.display = 'flex'
+
+        // Alterando a width do ContainerRight
+        document.getElementById('ContainerRight').style.width = `calc(100% - 400px})`
+    }
+    
     return(
         <div id="ContainerRight">
-          oi  
+            {/* button start Game */}
+          <button id='btn-Start-Game' onClick={startGame}>Game Start</button>
+
+          
         </div>
     )
 }
